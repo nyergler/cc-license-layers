20bb19ee01ae6446d179eb9b3affe1e6109e0f78
Nathan R. Yergler <nathan@yergler.net>
2011-01-07 10:01:27 -0800

    Initial import of license layer visualization.

---
+++ layers.js
@@ -0,0 +1,56 @@
+/**
+ *
+ * License Layers Demo Javascript
+ * 
+ * copyright 2010, Creative Commons, Nathan Yergler, Alex Roberts
+ * licensed to the public under CC BY 3.0 Unported
+ * <http://creativecommons.org/licenses/by/3.0/>.
+ *
+ * Relies on jquery (tested with 1.4.4).
+ * 
+ **/
+
+$(document).ready(function() {
+
+    $(".layer").click(function(e) {
+	
+	if ($(this).hasClass("focused")) {
+	    // the element was previously focused, return to the stack
+            $(this).removeClass("focused");
+	    $("#" + $(this).attr("id") + "Desc").removeClass("focusedtext");
+	    
+	    $("#intro").addClass("focusedtext");
+	    
+	} else if ($(this).hasClass("exploded")) {
+	    // focus on this layer
+	    
+	    // -- first unfocus other layers
+            $(".layer").removeClass("focused");
+            $(".description").removeClass("focusedtext");
+	    
+	    // -- focus on the layer and show the description
+            $(this).addClass("focused");
+	    $("#" + $(this).attr("id") + "Desc").addClass("focusedtext");
+	} else  {
+	    // layers are still stacked
+            $(".layer").addClass("exploded");
+	    
+	    // after a delay (for transition), hide the CC layer
+	    window.setTimeout(function() {
+		$("#cc").hide();
+	    }, 1000);
+
+	}
+    }); // on layer click    
+    
+    $(".layer").hover(
+	function(e) {	
+	    $(".layer[id!='" + $(this)[0].id + "']").toggleClass("dimmed");
+	    $(this).toggleClass("hover");
+	});
+
+    $("#getstarted").click(function(e) {
+	$("#cc").click();
+	e.preventDefault();
+    });
+});
